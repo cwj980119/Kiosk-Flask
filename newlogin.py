18c02792ba69803c9f7333c9818c84ab35ef4c5e
ksc1583 <kscksc1583@gmail.com>
2022-10-25 17:32:04 +0900

    login형식 맞춤
    count, working 삭제 등등

--- newlogin.py
+++ newlogin.py
@@ -11,20 +11,17 @@ class flasklogin():    # 구 Thread 현 flasklogin
         print("hello")
         
     def login(self):  # 구 run 현
-        
         detector = dlib.get_frontal_face_detector()
         #cam = cv2.VideoCapture(0, cv2.CAP_DSHOW) 삭제?
         
-        #count = 0
-        self.l = [0 for i in range(4)]
-        frame = cv2.imread('./image/temp.jpg',1)
-        #cv2.imshow('VideoFrame', frame) #사진 보이기 테스트
-        #cv2.waitKey(1000)
-        face = detector(frame)
         user_list = np.empty(shape=self.user_num)
-        print(self.user_num)
+        self.l = [0 for i in range(4)]
+        #print(self.user_num)
+        #while self.working:
+        frame = cv2.imread('./image/temp.jpg',1)
+        face = detector(frame)
         for f in face:
-            # dlib으로 얼굴 검출
+                # dlib으로 얼굴 검출
             cv2.rectangle(frame, (f.left(), f.top()), (f.right(), f.bottom()), (0, 0, 255), 1)
         
         if len(face) == 1:
@@ -32,32 +29,34 @@ class flasklogin():    # 구 Thread 현 flasklogin
             crop = cv2.resize(crop, (224, 224))
             image = np.array(crop)
             image = image.astype('float32') / 255
-            #
-            # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  ####gray
-            # image = np.expand_dims(image,-1)                 ####gray
+                #
+                # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  ####gray
+                # image = np.expand_dims(image,-1)                 ####gray
 
             image = np.expand_dims(image, 0)
-            print(image.shape)
-            a = load_model.predict(image)
-            print(a[0][np.argmax(a)])
-            #if count < 100:
-            #    count += 1
-            user_list += a[0]
-            #print(count)
+                #print(image.shape)
+            a = load_model.predict(image)   
+            #print(a[0][np.argmax(a)])
+            user_list = a[0]
+            print(a[0])
+            print(user_list)
         else:
             print("얼굴이 없습니다")
             return 0
                   
-        frame = cv2.flip(frame, 1) #좌우반전
-        cvt_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #색상공간변환함수
-        h, w, c = cvt_frame.shape
+        # frame = cv2.flip(frame, 1) #좌우반전
+        #cvt_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #색상공간변환함수
+        #h, w, c = cvt_frame.shape
         
-        if count > 50:
-            a = np.sort(user_list)[::-1]
-            for i in range(4):
-                self.l[i] = np.where(user_list==a[i])[0][0]
-            return 
-    
+        a = np.sort(user_list)[::-1]
+        for i in range(4):
+            self.l[i] = np.where(user_list==a[i])[0][0]
+            #    self.working = False
+        
+        print("end")
+        print(self.l)
+        #self.working = True
+        return
         
     def db_check(self):
         try:
@@ -66,7 +65,7 @@ class flasklogin():    # 구 Thread 현 flasklogin
             sql1 = "Select max(memberID) from sho"
             self.curs.execute(sql1)
             result = self.curs.fetchone()
-            self.user_num = result[0] + 1
+            self.user_num = result[0]
             print(self.user_num)
         except:
             print("DB 연결 실패")
