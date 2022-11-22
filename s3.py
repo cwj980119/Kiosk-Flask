eed1cafdbf56c5120b40121a1c77a82aedee25c3
ksc1583 <kscksc1583@gmail.com>
2022-11-22 22:04:08 +0900

    nonsignup_s3_get_object

--- s3.py
+++ s3.py
@@ -81,6 +81,24 @@ def s3_get_object(s3, bucket, object_name, file_name):
     
     return True
 
+def nonsignup_s3_get_object(s3, bucket, object_name, file_name):
+    '''
+    s3 bucket에서 지정 파일 다운로드
+    :param s3: 연결된 s3 객체(boto3 client)
+    :param bucket: 버킷명
+    :param object_name: s3에 저장된 object 명
+    :param file_name: 저장할 파일 명(path)
+    :return: 성공 시 True, 실패 시 False 반환
+    '''
+    try:
+        s3.download_file(bucket, object_name, file_name)
+        print("done")
+    except Exception as e:
+        print(e)
+        return False
+    
+    return True
+
 def s3_get_alldataset(s3,bucket):
     #모든 훈련용 데이터 다운
     count=0
